3cfdcd3574180930e8683c1cc94ffe8cc3ceadb8
Arystan <arystankaliakparov@gmail.com>
2020-10-01 21:42:40 +0600

    initialize react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Card, Container } from "react-bootstrap";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import MyNavBar from "./NavBar";
+
+const players = [
+  {
+    name: "Arystan"
+  },
+  {
+    name: "Adlet"
+  }
+]
+
+const Player = ({ name }) => {
+  return (
+    <Card style={{ width: '18rem', margin: 10, background: "yellow" }}>
+      <Card.Body>
+      <Card.Title>{name}</Card.Title>
+      </Card.Body>
+    </Card>
+  )
+}
 
 function App() {
+  console.log(players);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MyNavBar />
+      <Container>
+        {players.map((el) => <Player name={el.name} />)}
+      </Container>
+    </>
   );
 }
 
